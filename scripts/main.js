2d3572b292ced127888addb846f7e133da704772
Bryan <bmiller@tb.yearup.org>
2022-11-10 15:45:27 -0500

    navbar is set up in html js and css, want to upgrade later

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,12 @@ const navURLs = [
     { name: "Products", url: "products.html" },
 ];
 
+const navIcons = [
+    {classList: "fab fa-opencart px-3", url: "#"},
+    {classList: "far fa-user", url: "#"},
+    {classList: "subtitle px-3", url: "#"}
+]
+
 function navLink(item) {
     const div = document.createElement("div");
     div.classList.add("nav-block");
@@ -18,7 +24,23 @@ function navLink(item) {
     return a;
 }
 
+function createNavIcon(icon){
+    const span = document.createElement("span");
+    const a = document.createElement("a");
+    const i = document.createElement("i");
+    a.href = icon.url;
+    span.setAttribute("class", icon.classList);
+    span.appendChild(i);
+    a.appendChild(span);
+    
+    return a;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.getElementById("nav");
     navURLs.forEach(item => nav.appendChild(navLink(item)));
+
+    const form = document.createElement("form");
+    navIcons.forEach(icon => form.appendChild(createNavIcon(icon)));
+    nav.appendChild(form);
 });
