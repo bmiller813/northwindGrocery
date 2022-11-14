5360207f295e4cd728ddecc6f60da38c7bb6294b
Bryan <bmiller@tb.yearup.org>
2022-11-14 12:30:21 -0500

    Created a table for the categories search and started CSS for it

--- scripts/products.js
+++ scripts/products.js
@@ -4,14 +4,14 @@ function menuBarChange(x) {
 }
 
 const navURLs = [
-    {name: "Home", url: "index.html"},
+    { name: "Home", url: "index.html" },
     { name: "About Us", url: "#" },
 ];
 
 const navIcons = [
-    {classList: "fab fa-opencart px-3", url: "#"},
-    {classList: "far fa-user", url: "#"},
-    {classList: "subtitle px-3", url: "#"}
+    { classList: "fab fa-opencart px-3", url: "#" },
+    { classList: "far fa-user", url: "#" },
+    { classList: "subtitle px-3", url: "#" }
 ]
 
 function navLink(item) {
@@ -24,7 +24,7 @@ function navLink(item) {
     return a;
 }
 
-function createNavIcon(icon){
+function createNavIcon(icon) {
     const span = document.createElement("span");
     const a = document.createElement("a");
     const i = document.createElement("i");
@@ -32,7 +32,7 @@ function createNavIcon(icon){
     span.setAttribute("class", icon.classList);
     span.appendChild(i);
     a.appendChild(span);
-    
+
     return a;
 }
 
@@ -44,35 +44,60 @@ document.addEventListener("DOMContentLoaded", () => {
     navIcons.forEach(icon => form.appendChild(createNavIcon(icon)));
     nav.appendChild(form);
 });
+
 //   MAIN CODE
+
 const mode = document.getElementById("mode");
 const results = document.getElementById("results");
 let select = undefined; //global scope
+
+function productLink(p){
+    const d = document.createElement("div");
+    d.innerHTML = `<a href="details.html?id=${p.productId}"> ${p.productName}</a>`;
+    return d
+}
 function showAll() {
     results.innerHTML = "";
     fetch("http://localhost:8081/api/products")
         .then(response => response.json())
         .then(data => {
-            data.forEach(p => {
-                const d = document.createElement("div");
-                d.innerHTML = `<a href="details.html?id=${p.productId}"> ${p.productName}</a>`;
-                results.appendChild(d);
+            data.forEach(p => {  
+                results.appendChild((productLink(p)));
             })
         })
 }
+
+function createTd(product, property){
+    let td = document.createElement("td");
+    td.innerHTML = product[property];
+    return td;
+}
+
+function createRow(product, table) {
+    let row = document.createElement("tr");
+    //table.classList.add("")
+    row.appendChild(createTd(product, "productName"));
+    row.appendChild(createTd(product, "unitPrice"));
+    row.appendChild(createTd(product, "unitsInStock"));
+    row.appendChild(createTd(product, "supplier"));
+    table.appendChild(row);
+}
+
 function showResults() {
     results.innerHTML = "";
     const id = select.selectedOptions[0].value;
     fetch("http://localhost:8081/api/categories/" + id)
         .then(response => response.json())
         .then(data => {
-            data.forEach(p => {
-                const d = document.createElement("div");
-                d.innerHTML = `<a href="details.html?id=${p.productId}"> ${p.productName}</a>`;
-                results.appendChild(d);
+            const table = document.createElement("table");
+            //table.border = 1;
+            data.forEach(p => {  
+                createRow(p, table);
             })
+            results.appendChild(table);
         })
 }
+
 function createSelect() {
     select = document.createElement("select");
     fetch("http://localhost:8081/api/categories")
@@ -88,27 +113,6 @@ function createSelect() {
             mode.after(select); //insert new select after the mode select.
             //document.body.appendChild(select);
         })
-}
-
-//======================== cards ====================== 
-
-function parkCard(item){
-    let card; 
-    if(item.Visit != undefined){
-        card = document.createElement("a");
-        card.target = "_blank";
-        card.href = item.Visit
-    }else{
-        card = document.createElement("div");
-    }
-    card.classList.add("card");
-    card.innerHTML = item.LocationName;
-    return card;
-}
-
-function showCards(list, target){
-    target.innerHTML = ""; //CLEAR
-    list.forEach(item => target.appendChild(parkCard(item)));
 }
 
 //=======================================================
